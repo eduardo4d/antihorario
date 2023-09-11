5cf7c5fc0366d964fc50ee64a56eb7f7fa646de4
Eduardo <83039893+eduardo4d@users.noreply.github.com>
2023-09-11 14:16:32 -0300

    Update clock.js

--- assets/clock.js
+++ assets/clock.js
@@ -234,7 +234,7 @@ function arc(center, radius, t1, t2, fill = true) {
   });
 
   context.beginPath();
-  context.arc(center[0], center[1], radius, arcInit, arcEnd, counterclockwise);
+  context.arc(center[0], center[1], radius, arcInit, arcEnd).scale(-1, 1);
   if (fill) context.fill();
   else context.stroke();
 }
